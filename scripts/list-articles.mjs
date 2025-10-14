53e79052fdd8fc92144c38bbcd42e00f90e53e47
bouzuya <m@bouzuya.net>
2025-10-14 19:35:15 +0900

    Add `npm run list`

---
+++ scripts/list-articles.mjs
@@ -0,0 +1,22 @@
+import { readFileSync } from 'node:fs';
+import fm from 'front-matter';
+
+function main() {
+    for (const [date, slug] of Object.entries(JSON.parse(readFileSync('./articles.json')))) {
+        const content = readFileSync(`./articles/${slug}.md`, { encoding: 'utf-8' });
+        const attrs = fm(content).attributes;
+        const publishedAt = attrs['published_at'];
+        const pubdate = publishedAt.substring(0, 10);
+        const title = attrs['title'];
+
+        const line = [
+            '-',
+            `[${date}](articles/${slug}.md)`,
+            `([${pubdate}](https://zenn.dev/doctormate/articles/${slug}))`,
+            title
+        ].join(' ');
+        console.log(line);
+    }
+}
+
+main();
